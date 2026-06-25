df8b3537732f647a9183ed5895ee8f4a395d8dee
Ethan Pham <ethanpham652@gmail.com>
2026-06-24 19:41:43 -0500

    Add: is-anagram - submission-11

---
+++ Data Structures & Algorithms/is-anagram/submission-11.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # return sorted(s) == sorted(t);
+        if len(s) != len(t):
+            return False
+            
+        s_set = {};
+        t_set = {};
+
+        for i in s:
+            if i in s_set:
+                s_set[i] += 1
+            else:
+                s_set[i] = 1
+
+        for j in t:
+            if j in t_set:
+                t_set[j] += 1
+            else:
+                t_set[j] = 1
+        
+        if s_set == t_set:
+            return True
+        
+        return False
